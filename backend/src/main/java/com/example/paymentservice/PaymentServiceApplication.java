82d81020a1070947d519650b5550a8cd02b713cd
NikolinaPavkovic <pavkovicn@uns.ac.rs>
2022-04-11 15:38:36 +0200

    feat: paypal configuration

--- backend/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ backend/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.paymentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan({"com.example.paymentservice.config"})
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
---
+++ backend/src/main/java/com/example/paymentservice/model/Rent.java
@@ -0,0 +1,15 @@
+package com.example.paymentservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Rent {
+    private double price;
+    private String intent;
+}
